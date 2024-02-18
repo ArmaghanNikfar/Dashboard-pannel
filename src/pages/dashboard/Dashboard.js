4c58534be6d2473ddfea7efc3f50b2da2b57d63d
Armaghan Nikfar <armaghannikfar@gmail.com>
2024-02-18 15:51:21 +0330

    changes

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -16,20 +16,7 @@ import { useTheme } from "@material-ui/styles";
 import ApexCharts from 'apexcharts';
 import cardBackground from './images/card.jpg'
 import Dot from "../../components/Sidebar/components/Dot";
-// import {
-//   ResponsiveContainer,
-//   ComposedChart,
-//   AreaChart,
-//   LineChart,
-//   Line,
-//   Area,
-//   PieChart,
-//   Pie,
-//   Cell,
-//   YAxis,
-//   XAxis,
-// } from "recharts";
-//food logo
+
 import food from "./images/3565302-200.png";
 
 // services logo 
@@ -48,25 +35,12 @@ import useStyles from "./styles";
 import Widget from "../../components/Widget";
 import PageTitle from "../../components/PageTitle";
 import { Typography } from "../../components/Wrappers";
-// import mock from "./mock";
 import { CheckBox } from "@material-ui/icons";
-// import Table from "./components/Table/Table";
-// import BigStat from "./components/BigStat/BigStat";
-
-// const mainChartData = getMainChartData();
-// const PieChartData = [
-//   { name: "گروه A", value: 400, color: "primary" },
-//   { name: "گروه B", value: 300, color: "secondary" },
-//   { name: "گروه C", value: 300, color: "warning" },
-//   { name: "گروه D", value: 200, color: "success" },
-// ];
 
 export default function Dashboard(props) {
   var classes = useStyles();
   var theme = useTheme();
 
-  // local
-  //var [mainChartState, setMainChartState] = useState("monthly");
   useEffect(() => {
     const options = {
       colors: ['#2ea2fa', '#00e396', '#239b39', '#239b39'],
@@ -321,162 +295,6 @@ export default function Dashboard(props) {
         </Grid >
 
 
-
-        {/* <Grid item lg={3} md={4} sm={6} xs={12}>
-          <Widget title="تقسیم درآمد" upperTitle className={classes.card}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <ResponsiveContainer width="100%" height={144}>
-                  <PieChart>
-                    <Pie
-                      data={PieChartData}
-                      innerRadius={30}
-                      outerRadius={40}
-                      dataKey="value"
-                    >
-                      {PieChartData.map((entry, index) => (
-                        <Cell
-                          key={`cell-${index}`}
-                          fill={theme.palette[entry.color].main}
-                        />
-                      ))}
-                    </Pie>
-                  </PieChart>
-                </ResponsiveContainer>
-              </Grid>
-              <Grid item xs={6}>
-                <div className={classes.pieChartLegendWrapper}>
-                  {PieChartData.map(({ name, value, color }, index) => (
-                    <div key={color} className={classes.legendItemContainer}>
-                      <Dot color={color} />
-                      <Typography style={{ whiteSpace: "nowrap", fontSize: 12 }} >
-                        &nbsp;{name}&nbsp;
-                      </Typography>
-                      <Typography color="text" colorBrightness="secondary">
-                        &nbsp;{value}
-                      </Typography>
-                    </div>
-                  ))}
-                </div>
-              </Grid>
-            </Grid>
-          </Widget>
-        </Grid> */}
-        {/* <Grid item xs={12}>
-          <Widget
-            bodyClass={classes.mainChartBody}
-            header={
-              <div className={classes.mainChartHeader}>
-                <Typography
-                  variant="h5"
-                  color="text"
-                  colorBrightness="secondary"
-                >
-                  نمودار روزانه  
-                </Typography>
-                <div className={classes.mainChartHeaderLabels}>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="warning" />
-                    <Typography className={classes.mainChartLegentElement}>
-                      تبلت
-                    </Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>
-                      موبایل
-                    </Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="secondary" />
-                    <Typography className={classes.mainChartLegentElement}>
-                      دسکتاب
-                    </Typography>
-                  </div>
-                </div>
-                <Select
-                  value={mainChartState}
-                  onChange={e => setMainChartState(e.target.value)}
-                  input={
-                    <OutlinedInput
-                      labelWidth={0}
-                      classes={{
-                        notchedOutline: classes.mainChartSelectRoot,
-                        input: classes.mainChartSelect,
-                      }}
-                    />
-                  }
-                  autoWidth
-                >
-                  <MenuItem value="daily">روزانه</MenuItem>
-                  <MenuItem value="weekly">هفتگی</MenuItem>
-                  <MenuItem value="monthly">ماهانه</MenuItem>
-                </Select>
-              </div>
-            }
-          >
-            <ResponsiveContainer width="100%" minWidth={500} height={350}>
-              <ComposedChart
-                margin={{ top: 0, right: -15, left: -15, bottom: 0 }}
-                data={mainChartData}
-              >
-                <YAxis
-                  ticks={[0, 2500, 5000, 7500]}
-                  tick={{ fill: theme.palette.text.hint + "80", fontSize: 14 }}
-                  stroke={theme.palette.text.hint + "80"}
-                  tickLine={false}
-                />
-                <XAxis
-                  tickFormatter={i => i + 1}
-                  tick={{ fill: theme.palette.text.hint + "80", fontSize: 14 }}
-                  stroke={theme.palette.text.hint + "80"}
-                  tickLine={false}
-                />
-                <Area
-                  type="natural"
-                  dataKey="desktop"
-                  fill={theme.palette.background.light}
-                  strokeWidth={0}
-                  activeDot={false}
-                />
-                <Line
-                  type="natural"
-                  dataKey="mobile"
-                  stroke={theme.palette.primary.main}
-                  strokeWidth={2}
-                  dot={false}
-                  activeDot={false}
-                />
-                <Line
-                  type="linear"
-                  dataKey="tablet"
-                  stroke={theme.palette.warning.main}
-                  strokeWidth={2}
-                  dot={{
-                    stroke: theme.palette.warning.dark,
-                    strokeWidth: 2,
-                    fill: theme.palette.warning.main,
-                  }}
-                />
-              </ComposedChart>
-            </ResponsiveContainer>
-          </Widget>
-        </Grid> */}
-        {/* {mock.bigStat.map(stat => (
-          <Grid item md={4} sm={6} xs={12} key={stat.product}>
-            <BigStat {...stat} />
-          </Grid>
-        ))} */}
-        {/* <Grid item xs={12}>
-          <Widget
-            title="پشتیبانی درخواست ها"
-            upperTitle
-            noBodyPadding
-            bodyClass={classes.tableWidget}
-          >
-            <Table data={mock.table} />
-          </Widget>
-        </Grid> */}
       </Grid>
       <Grid container style={{ marginTop: "10px" }}>
         <div id="callDetailsChart" style={{ width: "700px" }}
@@ -594,41 +412,3 @@ export default function Dashboard(props) {
   );
 }
 
-// #######################################################################
-// function getRandomData(length, min, max, multiplier = 10, maxDiff = 10) {
-//   var array = new Array(length).fill();
-//   let lastValue;
-
-//   return array.map((item, index) => {
-//     let randomValue = Math.floor(Math.random() * multiplier + 1);
-
-//     while (
-//       randomValue <= min ||
-//       randomValue >= max ||
-//       (lastValue && randomValue - lastValue > maxDiff)
-//     ) {
-//       randomValue = Math.floor(Math.random() * multiplier + 1);
-//     }
-
-//     lastValue = randomValue;
-
-//     return { value: randomValue };
-//   });
-// }
-
-// function getMainChartData() {
-//   var resultArray = [];
-//   var tablet = getRandomData(31, 3500, 6500, 7500, 1000);
-//   var desktop = getRandomData(31, 1500, 7500, 7500, 1500);
-//   var mobile = getRandomData(31, 1500, 7500, 7500, 1500);
-
-//   for (let i = 0; i < tablet.length; i++) {
-//     resultArray.push({
-//       tablet: tablet[i].value,
-//       desktop: desktop[i].value,
-//       mobile: mobile[i].value,
-//     });
-//   }
-
-//   return resultArray;
-// }
